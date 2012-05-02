d358854cc9b5aa3f0603b6075209b37eecdc3d6c
khkim <moon6pence@gmail.com>
2012-05-03 02:24:16 +0900

    Add gui script for viewing algorithm result

--- TextRank/textrank.py
+++ TextRank/textrank.py
@@ -9,70 +9,91 @@ from pygraph.classes.digraph import digraph
 from pygraph.algorithms.pagerank import pagerank
 from pygraph.classes.exceptions import AdditionError
 
-import urllib
-from readability.readability import Document
+def textrank(text):
+	def filter_for_tags(tagged, tags=['NN', 'JJ', 'NNP']):
+		return [item for item in tagged if item[1] in tags]
 
-def get_article(url):
-	html = urllib.urlopen(url).read()
-	document = Document(html)
-	readable_article = document.summary()
-	return nltk.clean_html(readable_article)
+	def normalize(tagged):
+		return [(item[0].replace('.', ''), item[1]) for item in tagged]
 
-def filter_for_tags(tagged, tags=['NN', 'JJ', 'NNP']):
-    return [item for item in tagged if item[1] in tags]
+	def unique_everseen(iterable, key=None):
+		seen = set()
+		seen_add = seen.add
+		if key is None:
+			for element in itertools.ifilterfalse(seen.__contains__, iterable):
+				seen_add(element)
+				yield element
+		else:
+			for element in iterable:
+				k = key(element)
+				if k not in seen:
+					seen_add(k)
+					yield element
 
-def normalize(tagged):
-    return [(item[0].replace('.', ''), item[1]) for item in tagged]
+	text = nltk.word_tokenize(text)
+	tagged = nltk.pos_tag(text)
+	tagged = filter_for_tags(tagged)
+	tagged = normalize(tagged)
 
-def unique_everseen(iterable, key=None):
-    "List unique elements, preserving order. Remember all elements ever seen."
-    # unique_everseen('AAAABBBCCDAABBB') --> A B C D
-    # unique_everseen('ABBCcAD', str.lower) --> A B C D
-    seen = set()
-    seen_add = seen.add
-    if key is None:
-        for element in itertools.ifilterfalse(seen.__contains__, iterable):
-            seen_add(element)
-            yield element
-    else:
-        for element in iterable:
-            k = key(element)
-            if k not in seen:
-                seen_add(k)
-                yield element
+	unique_word_set = unique_everseen([x[0] for x in tagged])
 
-url = "http://online.wsj.com/article/SB10001424052702303772904577333392094289110.html?mod=WSJASIA_hps_MIDDLESixthNews"
+	graph = digraph()
+	graph.add_nodes(list(unique_word_set))
 
-text = get_article(url)
-text = nltk.word_tokenize(text)
+	window_start = 0
+	window_end = 2
 
-tagged = nltk.pos_tag(text)
-tagged = filter_for_tags(tagged)
-tagged = normalize(tagged)
+	while 1:
+		window_words = tagged[window_start:window_end]
+		if len(window_words) == 2:
+			# print window_words
+			try:
+				graph.add_edge((window_words[0][0], window_words[1][0]))
+			except AdditionError, e:
+				# print 'already added %s, %s' % ((window_words[0][0], window_words[1][0]))
+				pass
+		else:
+			break
 
-unique_word_set = unique_everseen([x[0] for x in tagged])
+		window_start += 1
+		window_end += 1
 
-graph = digraph()
-graph.add_nodes(list(unique_word_set))
+	calculated_page_rank = pagerank(graph)
+	di = sorted(calculated_page_rank.iteritems(), key=itemgetter(1), reverse=1)
 
-window_start = 0
-window_end = 2
+	log = "\n".join([str(item[0]) + " : " + str(item[1]) for item in di])
+	tags = ", ".join([str(item[0]) for item in di][:5])
 
-while 1:
-    window_words = tagged[window_start:window_end]
-    if len(window_words) == 2:
-        print window_words
-        try:
-            graph.add_edge((window_words[0][0], window_words[1][0]))
-        except AdditionError, e:
-            print 'already added %s, %s' % ((window_words[0][0], window_words[1][0]))
-    else:
-        break
+	return log, tags
 
-    window_start += 1
-    window_end += 1
+def run(index):
+	print "\nIndex " + str(index)
 
-calculated_page_rank = pagerank(graph)
-di = sorted(calculated_page_rank.iteritems(), key=itemgetter(1))
-for k, g in itertools.groupby(di, key=itemgetter(1)):
-    print k, map(itemgetter(0), g)
+	dirname = "data/%04d" % index
+
+	# read english article
+	try:
+		text = open(dirname + "/article_en.txt").read()
+	except IOError:
+		print "No article file"
+		return
+
+	# run textrank
+	log, tags = textrank(text)
+	print tags
+
+	# write log
+	output = open(dirname + "/log.txt", 'w')
+	output.write(log)
+	output.close()
+
+	# write tags
+	output = open(dirname + "/tags.txt", 'w')
+	output.write(tags)
+	output.close()
+
+if __name__ == '__main__':
+	for index in range(100, 1000):
+		run(index)
+
+	#run(0)
---
+++ TextRank/viewer.py
@@ -0,0 +1,205 @@
+import wx
+import sys
+import re
+import threading
+import textrank
+
+if hasattr(sys, "setdefaultencoding"): 
+	sys.setdefaultencoding('mbcs')
+
+class MyApp(wx.App):
+	def OnInit(self):
+		frame = MyFrame()
+		frame.Show(True)
+		self.SetTopWindow(frame)
+		wx.STAY_ON_TOP
+
+		return True
+	   
+class MyStatusBar(wx.StatusBar):
+	def __init__(self, parent):
+		wx.StatusBar.__init__(self,parent,-1)
+		self.SetFieldsCount(1)
+		self.SetStatusText('NLPwithPyton Group 3'.decode("utf-8", 'replace').encode("cp949","replace"),0)
+	   
+class MyFrame(wx.Frame):
+	def __init__(self):
+		wx.Frame.__init__(self, None, -1, "Text Rank Viewer", size=(1010, 700), style=wx.DEFAULT_FRAME_STYLE|wx.NO_FULL_REPAINT_ON_RESIZE)
+		self.sizeChanged = False
+
+		menuFile = wx.Menu()
+		menuFile.Append(1, "&About...")
+		menuFile.AppendSeparator()
+		menuFile.Append(2, "E&xit")
+				
+		menuBar = wx.MenuBar()
+		menuBar.Append(menuFile, "&File")
+		self.SetMenuBar(menuBar)
+		
+		panel = wx.Panel(self, -1)
+		panel.SetBackgroundColour(wx.Color(255,255,220))
+		
+		self.statusBar = MyStatusBar(self)
+		self.SetStatusBar(self.statusBar)
+		
+		self.p_num = wx.TextCtrl(panel, -1, "", pos=(8, 10), size=(40,20))
+		wx.StaticText(panel, -1, "URL", pos=(270, 14), size=(25,20))
+		self.p_url = wx.TextCtrl(panel, -1, "", pos=(295, 10), size=(690,22))
+		self.p_url.SetEditable(False) 
+		
+		self.go = wx.Button(panel, -1, "Go".decode("utf-8", 'replace').encode("cp949","replace"), pos=(56, 10), size=(63,22))
+		self.prev = wx.Button(panel, -1, "Prev".decode("utf-8", 'replace').encode("cp949","replace"), pos=(124, 10), size=(63,22))
+		self.next = wx.Button(panel, -1, "Next".decode("utf-8", 'replace').encode("cp949","replace"), pos=(192, 10), size=(63,22))
+		
+		self.eng = wx.Button(panel, -1, "English".decode("utf-8", 'replace').encode("cp949","replace"), pos=(56, 57), size=(63,22))
+		self.kor = wx.Button(panel, -1, "Korean".decode("utf-8", 'replace').encode("cp949","replace"), pos=(124, 57), size=(63,22))
+		self.log = wx.Button(panel, -1, "Log".decode("utf-8", 'replace').encode("cp949","replace"), pos=(192, 57), size=(63,22))
+				
+		wx.StaticText(panel, -1, "Title", pos=(8, 90), size=(40,20))
+		self.p_title = wx.TextCtrl(panel, -1, "", pos=(56, 87), size=(929,22))
+		self.p_title.SetEditable(False) 
+		wx.StaticText(panel, -1, "Article", pos=(8, 115), size=(40,20))
+		self.p_article = wx.TextCtrl(panel, -1, "", pos=(56, 112), size=(929,445),style=wx.TE_MULTILINE)
+		self.p_article.SetEditable(False)
+		
+		wx.StaticText(panel, -1, "TagOrg", pos=(8, 565), size=(40,20))
+		self.p_tags_org = wx.TextCtrl(panel, -1, "", pos=(56, 562), size=(929,22))
+		self.p_tags_org.SetEditable(False)
+		wx.StaticText(panel, -1, "Tag", pos=(8, 590), size=(40,20))		
+		self.p_tags = wx.TextCtrl(panel, -1, "", pos=(56, 587), size=(861,22))
+		self.p_tags.SetEditable(False)
+		self.tag_update = wx.Button(panel, -1, "Update".decode("utf-8", 'replace').encode("cp949","replace"), pos=(922, 587), size=(63,22))
+		
+		self.Bind(wx.EVT_MENU, self.OnAbout, id=1)
+		self.Bind(wx.EVT_MENU, self.OnQuit, id=2)
+		
+		self.Bind(wx.EVT_BUTTON, self.OnPrev, self.prev)
+		self.Bind(wx.EVT_BUTTON, self.OnNext, self.next)
+		self.Bind(wx.EVT_BUTTON, self.OnGo, self.go)
+
+		self.Bind(wx.EVT_BUTTON, self.OnEng, self.eng)
+		self.Bind(wx.EVT_BUTTON, self.OnKor, self.kor)
+		self.Bind(wx.EVT_BUTTON, self.OnLog, self.log)
+		
+		self.Bind(wx.EVT_BUTTON, self.OnTag_Update, self.tag_update)
+		
+		self.num = 0
+		self.select = "english"
+
+		self.Update()
+		
+	def OnQuit(self, event):
+		self.Close()
+         
+	def OnAbout(self, event):
+		wx.MessageBox("You can see information about the TextRank".decode("utf-8", 'replace').encode("cp949","replace"), 
+			"About...", wx.OK | wx.ICON_INFORMATION, self)
+                
+	def OnPrev(self, event):
+		if self.num == 0:
+			self.num = 999
+		else:
+			self.num -= 1
+
+		self.Update()
+			
+	def OnNext(self, event):
+		if self.num == 999:
+			self.num = 0
+		else:
+			self.num += 1
+
+		self.Update()
+	
+	def OnGo(self, event):
+		if self.p_num.GetValue().isdigit():
+			self.num = int(float(self.p_num.GetValue()))
+		else:
+			self.num = 0
+
+		self.Update()
+
+	def OnEng(self, event):
+		self.select = "english"
+		self.Update()
+
+	def OnKor(self, event):
+		self.select = "korean"
+		self.Update()
+	
+	def OnLog(self, event):
+		self.select = "log"
+		self.Update()	
+
+	def OnTag_Update(self, event):
+		th=threading.Thread(target=self.Tag_make)
+		th.start()
+		
+	def Tag_make(self):
+		textrank.run(self.num)
+		self.Update()
+
+	def Update(self):
+		path = "data/%04d/" % self.num
+							
+		try:
+			url_en = open(path+"url_en.txt").read()
+		except IOError:
+			url_en = ""
+
+		try:
+			article_ko = open(path+"article_ko.txt").read()
+		except IOError:
+			article_ko = ""
+
+		try:
+			article_en = open(path+"article_en.txt").read()
+		except IOError:
+			article_en = ""		
+
+		try:
+			log = open(path+"log.txt").read()
+		except IOError:
+			log = ""	
+
+		try:		
+			title = open(path+"title.txt").read()
+		except IOError:
+			title = ""
+
+		try:
+			tags_org = open(path+"tags_org.txt").read()
+		except IOError:
+			tags_org = ""
+
+		try:
+			tags = open(path+"tags.txt").read()
+		except IOError:
+			tags = ""
+		
+		self.p_num.SetValue("%04d" % self.num)
+		self.p_url.SetValue(url_en.decode("utf-8", 'replace').encode("cp949","replace"))
+		self.p_title.SetValue(title.decode("utf-8", 'replace').encode("cp949","replace"))
+		self.p_tags_org.SetValue(tags_org.decode("utf-8", 'replace').encode("cp949","replace"))
+		self.p_tags.SetValue(tags.decode("utf-8", 'replace').encode("cp949","replace"))	
+		
+		article_en = self.Beautifulize(article_en)
+		article_ko = self.Beautifulize(article_ko)
+
+		if self.select == "english":
+			self.p_article.SetValue(article_en.decode("utf-8", 'replace').encode("cp949","replace"))
+		elif self.select == "korean":
+			self.p_article.SetValue(article_ko.decode("utf-8", 'replace').encode("cp949","replace"))
+		elif self.select == "log":
+			self.p_article.SetValue(log.decode("utf-8", 'replace').encode("cp949","replace"))
+	
+	def Beautifulize(self, text):
+		patturn = "(\n)+( )*(\n)+( )*((\n)+( )*)*"
+		text = re.sub(patturn, "\n ", text)
+		text = re.sub(" ( )+", " ", text)
+		text = re.sub("\n ", "\n\n ", text)			
+		return text
+
+if __name__ == '__main__':
+	app = MyApp(False)
+	app.MainLoop()
