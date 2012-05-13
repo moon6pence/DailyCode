d74868724876a8bbfb0f8bc8132fb824fee36251
khkim <moon6pence@gmail.com>
2012-05-13 15:23:40 +0900

    Download Korean article + tags

--- TextRank/article_ko.py
+++ TextRank/article_ko.py
@@ -1,29 +1,78 @@
 # -*- coding: utf-8 -*-
+import re
+import os
 import nltk
+
 import urllib
 import codecs
 from readability.readability import Document
 from lxml import etree
 
+input = open('urllist.txt')
+urllist = [url for url in input]
+
 def run(index):
-	print "\nIndex %d" % index
+	print "Index %d" % index
 
-	dirname = "data/%04d" % index
+	# make directories
+	dir_name = "data/%04d" % index
+	if not os.path.exists(dir_name):
+		os.mkdir(dir_name)
 
-	file_url = dirname + "/url_ko.txt"
-	url = open(file_url).read()
-	html = urllib.urlopen(url).read()
-	document = Document(html)
+	# korean article url
+	url_ko = urllist[index]
+
+	# write url_ko.txt
+	output = open(dir_name + "/url_ko.txt", 'w')
+	output.write(url_ko)
+	output.close()
 	
+	# load html
+	html = urllib.urlopen(url_ko).read()
+
+	# apply readability
+	document = Document(html)
 	article = nltk.clean_html(document.summary())
-	file_article = dirname + "/article_ko.txt"
-	output = codecs.open(file_article, 'w', 'utf-8')
+
+	# replace some characters
+	article = re.sub('&#13;', '\n', article)	
+
+	# write article_ko.txt
+	output = codecs.open(dir_name + "/article_ko.txt", 'w', 'utf-8')
 	output.write(article)
 	output.close()
 
-	print "Length: %d" % len(article)
+	# parse tags in html
+	dom = etree.HTML(html)
+	tags = dom.xpath("//div[@class='postcats']/ul/li/a")
+	text_tags = ', '.join([tag.text for tag in tags])
 
-for index in range(0, 1000):
-	run(index)
+	# write tags_org.txt
+	output = codecs.open(dir_name + "/tags_org.txt", 'w', 'utf-8')
+	output.write(text_tags)
+	output.close()
+
+	# parse english article url
+	links = dom.xpath("//div[@class='articlePage']/p/a")
+
+	url_en = ""
+	for link in links:
+		if link.attrib.has_key('href'):
+			url_en = link.attrib['href']
+
+	# write url_en.txt
+	output = open(dir_name + "/url_en.txt", 'w')
+	output.write(url_en)
+	output.close()
 
 #run(0)
+
+for index in range(0, 1000):
+	try:
+		run(index)
+	except Exception:
+		try:
+			run(index)
+		except Exception:
+			print "----- Failed"
+
