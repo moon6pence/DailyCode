cab6012aff9490c3654ce33dee9195c265aa45e4
khkim <moon6pence@gmail.com>
2012-05-13 15:25:01 +0900

    Fixed encoding problem while downloading English article

--- TextRank/article_en.py
+++ TextRank/article_en.py
@@ -1,67 +1,47 @@
 # -*- coding: utf-8 -*-
-import nltk
 import urllib
-import codecs
 from readability.readability import Document
-from lxml import etree
+import nltk
+import codecs
+import re
 
 def run(index):
-	print "\nIndex %d" % index
-
+	print "Index %d" % index
 	dirname = "data/%04d" % index
 
-	file_url = dirname + "/url_en.txt"
-	url = open(file_url).read()
-	html = urllib.urlopen(url).read()
+	# url of english article
+	url = open(dirname + "/url_en.txt").read()
+
+	# download html
+	html = urllib.urlopen(url).read().decode('latin-1')
+
+	# apply readability
 	document = Document(html)
-	
-	title = unicode(document.title()).encode('utf-8')
-	file_title = dirname + "/title.txt"
-	output = open(file_title, 'w')
-	output.write(title)
-	output.close()
+	article = document.summary()
+	article = nltk.clean_html(article)
 
-	print title
+	# replace latin characters
+	article = re.sub(u'&#13;', u'\n', article)
+	article = re.sub(u'\x92', u'`', article)
+	article = re.sub(u'\x96', u'-', article)
 
-	article = process_replace(nltk.clean_html(document.summary()))
-	file_article = dirname + "/article_en.txt"
-	output = codecs.open(file_article, 'w', 'utf-8')
+	# article_en.txt
+	output = codecs.open(dirname + "/article_en.txt", 'w', encoding='ascii', errors='ignore')
 	output.write(article)
 	output.close()
 
-	# parse html
-	dom = etree.HTML(html)
-	links = dom.xpath("//div[@class='postcats']/ul/li/a")
-	tags = ", ".join([link.text for link in links])
-
-	file_tags = dirname + "/tags_org.txt"
-	output = open(file_tags, "w")
-	output.write(tags)
+	# title.txt
+	output = codecs.open(dirname + "/title.txt", 'w', encoding='ascii', errors='ignore')
+	output.write(document.title())
 	output.close()
 
-def process_replace(text):
-	return text.replace("&#13;", "")
+#run(949)
 
-#for index in range(966, 1000):
-#	run(index)
-
-#run(0)
-
-'''
-for index in range(1000):
+for index in range(0, 1000):
 	try:
-		print "Index " + str(index)
-
-		filename = "data/%04d/article_en.txt" % index
-		input = open(filename)
-		article = input.read()
-		input.close()
-
-		article = process_replace(article)
-		
-		output = open(filename, "w")
-		output.write(article)
-		output.close()
-	except IOError:
-		pass
-'''
+		run(index)
+	except Exception:
+		try:
+			run(index)
+		except Exception:
+			print "----- Failed"
