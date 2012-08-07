cdb088ae390f56dbe357d94843d17e2dcc65bd29
moon6pence <moon6pence@gmail.com>
2012-08-07 23:49:26 +0900

    Update basic vector classes to my code (only some coding inventions are updated)

--- OpenGL_ES2/Classes/Matrix.hpp
+++ OpenGL_ES2/Classes/Matrix.hpp
@@ -2,39 +2,42 @@
 #include "Vector.hpp"
 
 template <typename T>
-struct Matrix2 {
+struct Matrix2
+{
     Matrix2()
     {
         x.x = 1; x.y = 0;
         y.x = 0; y.y = 1;
     }
+    
     Matrix2(const T* m)
     {
         x.x = m[0]; x.y = m[1];
         y.x = m[2]; y.y = m[3];
     }
+    
     vec2 x;
     vec2 y;
 };
 
 template <typename T>
-struct Matrix3 {
+struct Matrix3
+{
     Matrix3()
     {
         x.x = 1; x.y = 0; x.z = 0;
         y.x = 0; y.y = 1; y.z = 0;
         z.x = 0; z.y = 0; z.z = 1;
     }
+    
     Matrix3(const T* m)
     {
         x.x = m[0]; x.y = m[1]; x.z = m[2];
         y.x = m[3]; y.y = m[4]; y.z = m[5];
         z.x = m[6]; z.y = m[7]; z.z = m[8];
     }
-    Matrix3(vec3 x, vec3 y, vec3 z) : x(x), y(y), z(z)
-    {
-    }
-    Matrix3 Transposed() const
+    
+    Matrix3 transposed() const
     {
         Matrix3 m;
         m.x.x = x.x; m.x.y = y.x; m.x.z = z.x;
@@ -42,17 +45,20 @@ struct Matrix3 {
         m.z.x = x.z; m.z.y = y.z; m.z.z = z.z;
         return m;
     }
-    const T* Pointer() const
+    
+    const T* pointer() const
     {
         return &x.x;
     }
+    
     vec3 x;
     vec3 y;
     vec3 z;
 };
 
 template <typename T>
-struct Matrix4 {
+struct Matrix4
+{
     Matrix4()
     {
         x.x = 1; x.y = 0; x.z = 0; x.w = 0;
@@ -60,6 +66,7 @@ struct Matrix4 {
         z.x = 0; z.y = 0; z.z = 1; z.w = 0;
         w.x = 0; w.y = 0; w.z = 0; w.w = 1;
     }
+    
     Matrix4(const Matrix3<T>& m)
     {
         x.x = m.x.x; x.y = m.x.y; x.z = m.x.z; x.w = 0;
@@ -67,6 +74,7 @@ struct Matrix4 {
         z.x = m.z.x; z.y = m.z.y; z.z = m.z.z; z.w = 0;
         w.x = 0; w.y = 0; w.z = 0; w.w = 1;
     }
+    
     Matrix4(const T* m)
     {
         x.x = m[0];  x.y = m[1];  x.z = m[2];  x.w = m[3];
@@ -74,6 +82,7 @@ struct Matrix4 {
         z.x = m[8];  z.y = m[9];  z.z = m[10]; z.w = m[11];
         w.x = m[12]; w.y = m[13]; w.z = m[14]; w.w = m[15];
     }
+    
     Matrix4 operator * (const Matrix4& b) const
     {
         Matrix4 m;
@@ -95,21 +104,14 @@ struct Matrix4 {
         m.w.w = w.x * b.x.w + w.y * b.y.w + w.z * b.z.w + w.w * b.w.w;
         return m;
     }
-    Vector4<T> operator * (const Vector4<T>& b) const
-    {
-        Vector4<T> v;
-        v.x = x.x * b.x + x.y * b.y + x.z * b.z + x.w * b.w;
-        v.y = y.x * b.x + y.y * b.y + y.z * b.z + y.w * b.w;
-        v.z = z.x * b.x + z.y * b.y + z.z * b.z + z.w * b.w;
-        v.w = w.x * b.x + w.y * b.y + w.z * b.z + w.w * b.w;
-        return v;
-    }
+    
     Matrix4& operator *= (const Matrix4& b)
     {
         Matrix4 m = *this * b;
         return (*this = m);
     }
-    Matrix4 Transposed() const
+    
+    Matrix4 transposed() const
     {
         Matrix4 m;
         m.x.x = x.x; m.x.y = y.x; m.x.z = z.x; m.x.w = w.x;
@@ -118,7 +120,8 @@ struct Matrix4 {
         m.w.x = x.w; m.w.y = y.w; m.w.z = z.w; m.w.w = w.w;
         return m;
     }
-    Matrix3<T> ToMat3() const
+    
+    Matrix3<T> toMat3() const
     {
         Matrix3<T> m;
         m.x.x = x.x; m.y.x = y.x; m.z.x = z.x;
@@ -126,24 +129,18 @@ struct Matrix4 {
         m.x.z = x.z; m.y.z = y.z; m.z.z = z.z;
         return m;
     }
-    const T* Pointer() const
+    
+    const T* pointer() const
     {
         return &x.x;
     }
-    static Matrix4<T> Identity()
+    
+    static Matrix4<T> identity()
     {
         return Matrix4();
     }
-    static Matrix4<T> Translate(const Vector3<T>& v)
-    {
-        Matrix4 m;
-        m.x.x = 1; m.x.y = 0; m.x.z = 0; m.x.w = 0;
-        m.y.x = 0; m.y.y = 1; m.y.z = 0; m.y.w = 0;
-        m.z.x = 0; m.z.y = 0; m.z.z = 1; m.z.w = 0;
-        m.w.x = v.x; m.w.y = v.y; m.w.z = v.z; m.w.w = 1;
-        return m;
-    }
-    static Matrix4<T> Translate(T x, T y, T z)
+    
+    static Matrix4<T> translate(T x, T y, T z)
     {
         Matrix4 m;
         m.x.x = 1; m.x.y = 0; m.x.z = 0; m.x.w = 0;
@@ -152,7 +149,8 @@ struct Matrix4 {
         m.w.x = x; m.w.y = y; m.w.z = z; m.w.w = 1;
         return m;
     }
-    static Matrix4<T> Scale(T s)
+    
+    static Matrix4<T> scale(T s)
     {
         Matrix4 m;
         m.x.x = s; m.x.y = 0; m.x.z = 0; m.x.w = 0;
@@ -161,33 +159,26 @@ struct Matrix4 {
         m.w.x = 0; m.w.y = 0; m.w.z = 0; m.w.w = 1;
         return m;
     }
-    static Matrix4<T> Scale(T x, T y, T z)
-    {
-        Matrix4 m;
-        m.x.x = x; m.x.y = 0; m.x.z = 0; m.x.w = 0;
-        m.y.x = 0; m.y.y = y; m.y.z = 0; m.y.w = 0;
-        m.z.x = 0; m.z.y = 0; m.z.z = z; m.z.w = 0;
-        m.w.x = 0; m.w.y = 0; m.w.z = 0; m.w.w = 1;
-        return m;
-    }
-    static Matrix4<T> Rotate(T degrees)
+    
+    static Matrix4<T> rotate(T degrees)
     {
         T radians = degrees * 3.14159f / 180.0f;
         T s = std::sin(radians);
         T c = std::cos(radians);
         
-        Matrix4 m = Identity();
+        Matrix4 m = identity();
         m.x.x =  c; m.x.y = s;
         m.y.x = -s; m.y.y = c;
         return m;
     }
-    static Matrix4<T> Rotate(T degrees, const vec3& axis)
+    
+    static Matrix4<T> rotate(T degrees, const vec3& axis)
     {
         T radians = degrees * 3.14159f / 180.0f;
         T s = std::sin(radians);
         T c = std::cos(radians);
         
-        Matrix4 m = Identity();
+        Matrix4 m = identity();
         m.x.x = c + (1 - c) * axis.x * axis.x;
         m.x.y = (1 - c) * axis.x * axis.y - axis.z * s;
         m.x.z = (1 - c) * axis.x * axis.z + axis.y * s;
@@ -199,22 +190,8 @@ struct Matrix4 {
         m.z.z = c + (1 - c) * axis.z * axis.z;
         return m;
     }
-    static Matrix4<T> Ortho(T left, T right, T bottom, T top, T near, T far)
-    {
-        T a = 2.0f / (right - left);
-        T b = 2.0f / (top - bottom);
-        T c = -2.0f / (far - near);
-        T tx = (right + left) / (right - left);
-        T ty = (top + bottom) / (top - bottom);
-        T tz = (far + near) / (far - near);
-        Matrix4 m;
-        m.x.x = a; m.x.y = 0; m.x.z = 0; m.x.w = tx;
-        m.y.x = 0; m.y.y = b; m.y.z = 0; m.y.w = ty;
-        m.z.x = 0; m.z.y = 0; m.z.z = c; m.z.w = tz;
-        m.w.x = 0; m.w.y = 0; m.w.z = 0; m.w.w = 1;
-        return m;
-    }
-    static Matrix4<T> Frustum(T left, T right, T bottom, T top, T near, T far)
+    
+    static Matrix4<T> frustum(T left, T right, T bottom, T top, T near, T far)
     {
         T a = 2 * near / (right - left);
         T b = 2 * near / (top - bottom);
@@ -229,26 +206,7 @@ struct Matrix4 {
         m.w.x = 0; m.w.y = 0; m.w.z = f; m.w.w = 1;
         return m;
     }
-    static Matrix4<T> LookAt(const Vector3<T>& eye,
-                             const Vector3<T>& target,
-                             const Vector3<T>& up)
-    {
-        Vector3<T> z = (eye - target).Normalized();
-        Vector3<T> x = up.Cross(z).Normalized();
-        Vector3<T> y = z.Cross(x).Normalized();
-        
-        Matrix4<T> m;
-        m.x = Vector4<T>(x, 0);
-        m.y = Vector4<T>(y, 0);
-        m.z = Vector4<T>(z, 0);
-        m.w = Vector4<T>(0, 0, 0, 1);
-        
-        Vector4<T> eyePrime = m * Vector4<T>(-eye, 1);
-        m = m.Transposed();
-        m.w = eyePrime;
-        
-        return m;
-    }
+    
     vec4 x;
     vec4 y;
     vec4 z;
